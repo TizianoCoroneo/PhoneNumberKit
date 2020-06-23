5df217129235f19bbf69f47d6a208d9ef514cc3a
Tiziano Coroneo <tiziano@ticketswap.com>
2020-06-23 11:57:12 +0200

    Implemented localized resources for Swift 5.3

--- Package.swift
+++ Package.swift
@@ -1,14 +1,26 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "PhoneNumberKit",
     products: [
-        .library(name: "PhoneNumberKit", targets: ["PhoneNumberKit"])
+        .library(
+            name: "PhoneNumberKit",
+            targets: ["PhoneNumberKit"])
     ],
     targets: [
-        .target(name: "PhoneNumberKit", path: "PhoneNumberKit", exclude: []),
-        .testTarget(name: "PhoneNumberKitTests", dependencies: ["PhoneNumberKit"], path: "PhoneNumberKitTests")
+        .target(
+            name: "PhoneNumberKit",
+            path: "PhoneNumberKit",
+            resources: [
+                .copy("PhoneNumberMetadata.json")
+            ]),
+
+        .testTarget(
+            name: "PhoneNumberKitTests",
+            dependencies: ["PhoneNumberKit"],
+            path: "PhoneNumberKitTests")
     ]
 )
+
